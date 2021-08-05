51c8d92d83c8c1cf725e5d26eba23435fbef7dc5
sandeepmchary <awssandeepchary@gmail.com>
2021-08-05 22:02:37 +0530

    added files in app

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask,jsonify
+import time
+app = Flask(__name__)
+app.route("/")
+def hello_world():
+    return jsonify({"Time to Call":time.time()})
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
