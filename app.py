b2557b086a27fb76abebc1c180f4dab869b719b7
sandeepmchary <awssandeepchary@gmail.com>
2021-08-05 22:13:24 +0530

    added files

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask,jsonify
-import time
+from datetime import datetime
+
+from flask import Flask
+    
 app = Flask(__name__)
-app.route("/")
-def hello_world():
-    return jsonify({"Time to Call":time.time()})
+
+@app.route("/")
+def home():
+    date = datetime.now()
+    return str(date)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
