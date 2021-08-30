3df33535d93841dc24e199cd9281ea66cda38762
sandeepmchary <awssandeepchary@gmail.com>
2021-08-30 19:31:29 +0530

    added

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from datetime import datetime
+from flask import request
+from flask import Flask, jsonify
 
-from flask import Flask
-    
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    date = datetime.now()
-    return str(date)
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
